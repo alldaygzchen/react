6b6deeddfb48421d807757c6473cddf5c3e5184b
Gonzalo Chen <gonzalochen66@gmail.com>
2025-02-02 12:39:22 +0800

    lorem done

--- 07-fundamental-projects/src/components/lorem/Lorem.jsx
+++ 07-fundamental-projects/src/components/lorem/Lorem.jsx
@@ -1,8 +1,12 @@
 import { useState } from 'react';
-import data from './data';
+// import data from './data';
 import { nanoid } from 'nanoid';
+import useFetch from './useFetch';
+
+const url = '../../../../lorem.json';
 
 const Lorem = () => {
+  const { isLoading: loading, isError: error, data: data } = useFetch(url, []);
   const [count, setCount] = useState(1);
   const [text, setText] = useState([]);
 
@@ -11,6 +15,15 @@ const Lorem = () => {
     let amount = parseInt(count);
     setText(data.slice(0, amount));
   };
+
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (error) {
+    return <h2>There was an error...</h2>;
+  }
+
   return (
     <section className="section-center">
       <h4>tired of boring lorem ipsum?</h4>
